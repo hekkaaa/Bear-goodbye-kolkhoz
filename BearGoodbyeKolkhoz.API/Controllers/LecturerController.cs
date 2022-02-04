4e2791779ad1a0be23b5802ba25dec30c07b7db9
AzarovRoman <azarovrom9215@gmail.com>
2022-02-04 10:44:17 +0300

    add structure

---
+++ BearGoodbyeKolkhoz.API/Controllers/LecturerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BearGoodbyeKolkhozProject.API.Models;
+
+namespace BearGoodbyeKolkhozProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LecturerController : Controller
+    {
+        [HttpGet]
+        public ActionResult RegistationLecturer()
+        {
+            return StatusCode(StatusCodes.Status201Created, new LecturerRegistrationInputModel { });
+        }
+    }
+}
---
+++ BearGoodbyeKolkhoz.API/Models/InputModels/LecturerRegistrationInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BearGoodbyeKolkhozProject.API.Models
+{
+    public class LecturerRegistrationInputModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
