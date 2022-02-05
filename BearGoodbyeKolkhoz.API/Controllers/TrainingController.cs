869b8abacea427dd2088a81dec01b41346cd4b6d
ll <jokzik@gmail.com>
2022-02-05 17:12:34 +0300

    t

---
+++ BearGoodbyeKolkhoz.API/Controllers/TrainingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BearGoodbyeKolkhozProject.API.Controllers
+{
+    public class TrainingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetTrainingById(int id)
+        {
+            return NotFound($"Лекция {id} не найдена!");
+
+            return Ok(new );
+        }
+
+        [HttpGet]
+        public ActionResult<List<>> GetTrainings()
+        {
+            return Ok(new List<>());
+        }
+
+    }
+}
---
+++ BearGoodbyeKolkhoz.API/Controllers/TrainingReviewController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BearGoodbyeKolkhozProject.API.Controllers
+{
+    public class TrainingReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetTrainingReviewById(int id)
+        {
+            return NotFound($"Лекция {id} не найдена!");
+
+            return Ok(new );
+        }
+
+        [HttpGet]
+        public ActionResult<List<>> GetTrainingReviews()
+        {
+            return Ok(new List<>());
+        }
+    }
+}
