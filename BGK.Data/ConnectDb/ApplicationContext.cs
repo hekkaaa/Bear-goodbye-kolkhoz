b3bdd45dbc96d7b4a7b9487ea1d8cb1aa92e0d88
Aleksandr <shohin-sasha1993@yandex.ru>
2022-02-10 22:54:38 +0300

    Rename

--- BGK.Data/ConnectDb/ApplicationContext.cs
+++ BGK.Data/ConnectDb/ApplicationContext.cs
@@ -26,7 +26,7 @@ namespace BearGoodbyeKolkhozProject.Data.ConnectDb
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=WIN-4PTG0MGAJ62\SQLEXPRESS;Database=testSS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-5HF0J14\SQLEXPRESS;Database=testSS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
