641ff218ce2f42d83b06edf7c888483f2d4d6ac2
Alexander Pototsky <silencemyalise@gmail.com>
2022-02-12 22:36:52 +0600

    add test GetAdminRepository

--- BearGoodbyeKolkhozProject.Data.Tests/AdminTraning.cs
+++ BearGoodbyeKolkhozProject.Data.Tests/AdminTraning.cs
@@ -1,30 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BearGoodbyeKolkhozProject.Data.ConnectDb;
+using BearGoodbyeKolkhozProject.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 
 namespace BearGoodbyeKolkhozProject.Data.Tests
 {
     public class AdminTraning
     {
+        private ApplicationContext _context;
+        private AdminRepository _adminRepository;
+        private TestData _testData;
+
         [SetUp]
         public void Setup()
         {
 
-            //var options = new DbContextOptionsBuilder<ApplicationContext>()
-            //   .UseInMemoryDatabase(databaseName: "Memory-DB")
-            //   .Options;
-            //_context = new ApplicationContext(options);
-            //_context.Database.EnsureDeleted();
-            //_context.Database.EnsureCreated();
-            //_trainingRepository = new TrainingRepository(_context);
-            //_testData = new TestData();
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+               .UseInMemoryDatabase(databaseName: "Memory-DB")
+               .Options;
+            _context = new ApplicationContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            _adminRepository = new AdminRepository(_context);
+            _testData = new TestData();
         }
 
 
         [Test]
         public void GetAdminByIdTests()
         {
+            //given
+            var training = _testData.GetTestAdmin();
+            _context.Add(training);
+            _context.SaveChanges();
 
-            Assert.AreEqual(1, 2);
+            //when
+            var act = _adminRepository.GetAdminById(training.Id);
+
+            //then
+            Assert.IsTrue(training.Id == act.Id);
+            Assert.IsNotNull(training);
+            Assert.IsNotNull(act.LastName);
+            Assert.IsNotNull(act.Gender);
+            Assert.IsNotNull(act.BirthDay);
+            Assert.IsNotNull(act.Email);
 
         }
 
