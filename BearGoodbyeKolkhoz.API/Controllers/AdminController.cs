bab90bd428ca03cbbb7c9e868c328e8f116c3679
Alexander Pototsky <silencemyalise@gmail.com>
2022-02-17 05:40:40 +0600

    preparing new methods for controller

--- BearGoodbyeKolkhoz.API/Controllers/AdminController.cs
+++ BearGoodbyeKolkhoz.API/Controllers/AdminController.cs
@@ -24,7 +24,7 @@ namespace BearGoodbyeKolkhozProject.API.Controllers
         [HttpGet("{id}")]
 
         public ActionResult<AdminOutputModel> GetAdminById(int id)
-        {   
+        {
             var model = _service.GetAdminById(id);
             var res = _mapper.Map<AdminOutputModel>(model);
 
@@ -40,7 +40,7 @@ namespace BearGoodbyeKolkhozProject.API.Controllers
         [HttpGet("all")]
         public ActionResult<List<AdminOutputModel>> GetAdminAll()
         {
-            var res = _service.GetAdminAll(); 
+            var res = _service.GetAdminAll();
 
             if (res == null)
             {
@@ -76,10 +76,61 @@ namespace BearGoodbyeKolkhozProject.API.Controllers
 
         [HttpPut("{id}")]
         public ActionResult<bool> UpdateAdmin(int id, [FromBody] AdminUpdateInputModel newItem)
-        {   
+        {
             var model = _mapper.Map<AdminModel>(newItem);
             var res = _service.UpdateAdminInfo(id, model);
             return Ok(res);
+        }
+
+        //api/admins/{id}/classroom
+        [HttpDelete("{id}/classroom")]
+        public ActionResult DeleteClassromById(int id)
+        {
+            // заглушка для удаления classroom
+            return Ok();
+        }
+
+        [HttpPut("{id}/classroom")]
+        public ActionResult UpdateClassroomById(AdminInsertInputModel newItem)
+        {
+            // заглушка для изменения classroom
+            return Ok();
+        }
+
+        //api/admins/{id}/traning
+        [HttpDelete("{id}/traning")]
+        public ActionResult DeleteTraningById(int id)
+        {
+            // заглушка для удаления traning
+            return Ok();
+        }
+
+        [HttpPut("{id}/traning")]
+        public ActionResult UpdateTraningById(AdminInsertInputModel newItem)
+        {
+            // заглушка для изменения traning
+            return Ok();
+        }
+
+        [HttpPut("{id}/lector/skills")]
+        public ActionResult UpdateSkillLectorById(int id)
+        {
+            // заглушка для изменения скилов lector
+            return Ok();
+        }
+
+        [HttpPut("{id}/user/ban")]
+        public ActionResult BanUserById(int id)
+        {
+            // заглушка для бана негодяев
+            return Ok();
+        }
+
+        [HttpPut("{id}/event/update")]
+        public ActionResult UpdateEventById(int id/*какой то инпут обьект*/)
+        {
+            // заглушка для редактирования эвентов
+            return Ok();
         }
 
     }
