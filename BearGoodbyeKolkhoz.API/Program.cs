ed80bc0835dda98c6aa8b8b2749e369dd038da48
AzarovRoman <azarovrom9215@gmail.com>
2022-02-22 21:36:12 +0300

    fix

--- BearGoodbyeKolkhoz.API/Program.cs
+++ BearGoodbyeKolkhoz.API/Program.cs
@@ -35,11 +35,8 @@ builder.Services.AddAutoMapper(typeof(APIMapperProfile), typeof(BusinessMapperPr
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
