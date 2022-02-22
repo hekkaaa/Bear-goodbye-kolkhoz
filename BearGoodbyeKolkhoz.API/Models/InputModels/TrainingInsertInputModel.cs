b8718011a9513488b57b882cc62f15cf1fcaa0f8
ll <jokzik@gmail.com>
2022-02-22 14:10:07 +0300

    start

--- BearGoodbyeKolkhoz.API/Models/InputModels/TrainingInsertInputModel.cs
+++ BearGoodbyeKolkhoz.API/Models/InputModels/TrainingInsertInputModel.cs
@@ -1,11 +1,18 @@
-﻿namespace BearGoodbyeKolkhozProject.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BearGoodbyeKolkhozProject.API.Models
 {
     public class TrainingInsertInputModel
     {
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Description { get; set; }
+        [Required]
         public int MembersCount { get; set; }
+        [Required]
         public int Duration { get; set; }
+        [Required]
         public int Price { get; set; }
     }
 }
