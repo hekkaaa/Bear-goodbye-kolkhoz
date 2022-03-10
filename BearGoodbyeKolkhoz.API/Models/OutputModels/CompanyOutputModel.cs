8d9c694b10e93193c97d31acee70b6644ffa0596
Alexander Pototsky <silencemyalise@gmail.com>
2022-03-10 17:31:19 +0600

    Add ValidationAttribute

--- BearGoodbyeKolkhoz.API/Models/OutputModels/CompanyOutputModel.cs
+++ BearGoodbyeKolkhoz.API/Models/OutputModels/CompanyOutputModel.cs
@@ -1,10 +1,13 @@
-﻿namespace BearGoodbyeKolkhozProject.API.Models.OutputModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BearGoodbyeKolkhozProject.API.Models.OutputModels
 {
     public class CompanyOutputModel
     {   
         public int Id { get; set; }
         public string Name { get; set; }
         public long Tin { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public bool IsDeleted { get; set; }
