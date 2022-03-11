e6f316e414571b19cf76d74c538a0d6d832e154e
Alexander Pototsky <silencemyalise@gmail.com>
2022-03-11 07:19:30 +0600

    amazing fix

--- BGK.Data/Entities/Company.cs
+++ BGK.Data/Entities/Company.cs
@@ -4,17 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace BearGoodbyeKolkhozProject.Data.Entities
 {
     [Table("Company")]
-    public class Company
+    public class Company : User
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
         public long Tin { get; set; }
-        public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public string Password { get; set; }
-        public Role Role { get; set; }
-        public bool IsDeleted { get; set; }
-
         public virtual ICollection<TrainingReview>? TrainingReviews { get; set; }
         public virtual ICollection<LecturerReview>? LecturerReviews { get; set; }
     }
